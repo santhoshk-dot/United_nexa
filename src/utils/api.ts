cbc7cac9debeb754f33881602b8d73d0a7a6cd92
sinthu2003 <sinthumuthuraj@gmail.com>
2025-12-19 13:41:56 +0530

    api link

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 import { loadingManager } from './loadingManager';
 
 // Use environment variable or default to localhost
-// export const API_URL = 'https://unitedtransportbackend-443415591723.asia-south1.run.app/api';
-export const API_URL = 'http://localhost:5000/api';
+export const API_URL = 'https://unitedtransportbackend-443415591723.asia-south1.run.app/api';
+// export const API_URL = 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
